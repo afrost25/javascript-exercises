1485e2e9bb03932d07b7755960cd841e5cd4b6f7
Austin Frost <frost.austin034@gmail.com>
2025-09-13 12:10:48 -0700

    Solution to JavaScript Problem Nine
    Implemented summation logic to return a summation between two numbers. Verified input of numbers by ensuring they are whole numbers and positive values.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(firstArg, secondArg) {
 
+    if(!Number.isInteger(firstArg))
+    {
+        return 'ERROR';
+    }
+
+    if(!Number.isInteger(secondArg))
+    {
+        return 'ERROR';
+    }
+
+    if(firstArg < 0)
+    {
+        return 'ERROR';
+    }
+
+    if(secondArg < 0)
+    {
+        return 'ERROR';
+    }
+
+    let result = 0;
+    let start = Math.min(firstArg, secondArg);
+    let end = Math.max(firstArg, secondArg);
+
+    for(let i = start; i <= end; i++)
+    {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
