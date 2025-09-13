28b7f40deeec4f8db261d537d3e0364f9a8ce493
Austin Frost <frost.austin034@gmail.com>
2025-09-13 12:31:03 -0700

    Solution for JavaScript Problem Eleven
    Implemented Fahrenheit and Celsius converter and rounded answer to one decimal place.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) 
+{
+  let rawCelsius = (5/9) * (fahrenheit - 32);
+  return roundDecimal(rawCelsius, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let rawFahrenheit = (9/5) * (celsius) + 32;
+  return roundDecimal(rawFahrenheit, 1);
 };
+
+function roundDecimal(num, places)
+{
+  return (Math.round(num * Math.pow(10, places)) / Math.pow(10, places));
+}
 
 // Do not edit below this line
 module.exports = {
